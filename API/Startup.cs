ddc3b3a46283254240007b3fd04f23bb3d5b3f13
RealOmerGal <Omergal156@gmail.com>
2022-11-05 12:21:29 +0200

    Commented some security settings

--- API/Startup.cs
+++ API/Startup.cs
@@ -74,7 +74,11 @@ namespace API
             }
             else
             {
-                app.UseHsts();
+                app.Use(async (context, next) =>
+               {
+                   context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+                   await next.Invoke();
+               });
             }
             var logger = app.ApplicationServices.GetRequiredService<ILogger<Startup>>();
             app.ConfigureExceptionHandler(logger, env);
